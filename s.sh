082e677037c14daa1752ae099cd1ab75fff37ef9
Francesco Giannelli <bassobassista@gmail.com>
2016-04-22 23:03:22 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get install -q -y build-essential zlib1g-dev libpcre3 libpcre3-dev unzip libssl-dev
+sudo apt-get update && sudo apt-get install -q -y build-essential zlib1g-dev libpcre3 libpcre3-dev unzip libssl-dev wget
 adduser --disabled-password --gecos "" b
 echo "b ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo -E -u b -H sh -c "/home/binginx/u.sh" 
