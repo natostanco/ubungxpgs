e44d3cb40e618ee1caaa9b1460bb36b00aeff9bb
Francesco Giannelli <bassobassista@gmail.com>
2016-04-22 22:32:05 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install -q -y build-essential zlib1g-dev libpcre3 libpcre3-dev unzip
+sudo apt-get update && sudo apt-get install -q -y build-essential zlib1g-dev libpcre3 libpcre3-dev unzip libssl-dev
 adduser b -D
 echo "b ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo -E -u b -H sh -c "/home/binginx/u.sh" 
